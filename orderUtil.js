e640b369becf69820a4fdadb68320b57d98d0576
meight <meightpc@gmail.com>
2021-03-05 20:23:22 +0530

    made buyNow and sortNow cleaner

--- orderUtil.js
+++ orderUtil.js
@@ -15,30 +15,24 @@ function updateOrders(orderData, userData, io) {
     var currentPrice = coincapApiUtil.getCurrentPrice();
     console.log("current price when trade happened ", coincapApiUtil.getCurrentPrice());
 
-    // TODO: these  tradde decisions should be made in orderutil.js, buybelow/buyAbove 
-    //      should have their decision and only then order is placed and current cash is reduced. 
-    var buyAmtIds = ['buyNowAmt', 'buyAtAmt'];
-    for(let i=0; i<buyAmtIds.length; i++) {
-        if(buyAmtIds[i] in orderData){
-            userData["currentFiat"] -= orderData[buyAmtIds[i]];
-            userData["holdings"]["BTCUSD"] += orderData[buyAmtIds[i]]/currentPrice;
-            console.log("current cash changed to ", userData["currentFiat"]);
-            console.log("new btc holding ", userData["holdings"]["BTCUSD"]);
-            break;
-        }
+    // buy now
+    if('buyNowAmt' in orderData){
+        var tempAmt = orderData['buyNowAmt'];
+        userData["currentFiat"] -= tempAmt;
+        userData["holdings"]["BTCUSD"] += tempAmt/currentPrice;
+        console.log("current cash changed to ", userData["currentFiat"]);
+        console.log("new btc holding ", userData["holdings"]["BTCUSD"]);        
     }
-    // TODO: if found in above buyAmt then skip below
-    var sortAmtIds = ['sortNowAmt', 'sortAtAmt'];
-    for(let i=0; i<sortAmtIds.length; i++) {
-        if(sortAmtIds[i] in orderData){
-            userData["currentFiat"]-= orderData[sortAmtIds[i]];
-            let temp = [orderData[sortAmtIds[i]]/currentPrice, currentPrice];
-            console.log(temp);
-            userData["sortedHoldings"]["BTCUSD"].push(temp);
-            console.log("current cash changed to ", userData["currentFiat"]);
-            console.log("new sorted btc holding ", orderData[sortAmtIds[i]]/currentPrice);
-            break;
-        }
+
+    // sort now
+    if('sortNowAmt' in orderData){
+        var tempAmt = orderData['sortNowAmt'];
+        userData["currentFiat"]-= tempAmt;
+        let temp = [tempAmt/currentPrice, currentPrice];
+        console.log(temp);
+        userData["sortedHoldings"]["BTCUSD"].push(temp);
+        console.log("current cash changed to ", userData["currentFiat"]);
+        console.log("new sorted btc holding ", tempAmt/currentPrice);
     }
 
     mongoUtil.updateUserdataToDb(userData);
