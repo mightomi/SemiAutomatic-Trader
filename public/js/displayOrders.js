1fa6ddb03b3ba746d890ff71024fe66b27b7ec13
meight <meightpc@gmail.com>
2021-03-08 14:45:17 +0530

    shows both buy and sort orders in Order Table

--- public/js/displayOrders.js
+++ public/js/displayOrders.js
@@ -11,9 +11,9 @@ function showPastOrdersHtml(pastOrders) {
     htmlTable += "<th>" + "Stock"+"</td>";
     htmlTable += "<th>" + "Time placed" + "</td>";
     htmlTable += "<th>" + "Order Completed" + "</td>";
-    htmlTable += "<th>" + "Buy Amount" + "</td>";
-    htmlTable += "<th>" + "Buy At" + "</td>";
-    // htmlTable += "<button> sell All</button>";
+    htmlTable += "<th>" + "Order Type" + "</td>";
+    htmlTable += "<th>" + "Amount" + "</td>";
+    htmlTable += "<th>" + "Execute when price At" + "</td>";
 
     htmlTable += "</tr>";
 
@@ -29,17 +29,32 @@ function showPastOrdersHtml(pastOrders) {
 
         htmlTable += "<td>" + index++ + "</td>";
         // htmlTable += "<td>" + order["crypto"] + "</td>";
-        htmlTable += "<td>" + "Bitcoin" + "</td>";
+        htmlTable += "<td>" + "Bitcoin" + "</td>"; // for now, simplicity
         htmlTable += "<td>" + dateTime + "</td>";
         htmlTable += "<td>" + order["orderCompleted"] + "</td>";
 
         if("buyNowAmt" in order) {
+            htmlTable +="<td>" + "Buy Now" + "</td>";
             htmlTable += "<td>" + order["buyNowAmt"] + "</td>";
             htmlTable += "<td>" + "-" + "</td>";
         }
-        else {
+        else if("buyAtAmt" in order) {
+            htmlTable += "<td>" + "Buy At" + "</td>";
             htmlTable += "<td>" + order["buyAtAmt"] + "</td>";
             htmlTable += "<td>" + order["buyAt"] + "</td>";
+            htmlTable += "<td>" + "<button> Cancel </button>" + "</td>";
+        }
+        
+        else if("sortNowAmt" in order) {
+            htmlTable +="<td>" + "Sort Now" + "</td>";
+            htmlTable += "<td>" + order["sortNowAmt"] + "</td>";
+            htmlTable += "<td>" + "-" + "</td>";
+        }
+        else if("sortAtAmt" in order) {
+            htmlTable +="<td>" + "Sort At" + "</td>";
+            htmlTable += "<td>" + order["sortAtAmt"] + "</td>";
+            htmlTable += "<td>" + order["sortAt"] + "</td>";
+            htmlTable += "<td>" + "<button> Cancel </button>" + "</td>";
         }
 
         htmlTable += "</tr>";
@@ -48,7 +63,7 @@ function showPastOrdersHtml(pastOrders) {
 
     htmlTable += "</table>"
 
-    $("#pastBuyOrderTable").html(htmlTable);
+    $("#pastOrderTable").html(htmlTable);
 }
 
 socket.on('pastOrders', function(pastOrders) {
