c86205d60cf6150013e1f7a641aa2e6d44544306
Avijitpal <77335137+Avijitpal@users.noreply.github.com>
2021-03-18 12:56:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,71 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const WebSocket = require('ws');
-
+var WebSocket = require('ws');
+var MongoClient = require('mongodb').MongoClient;
+var mongoDbUrl = "mongodb://localhost:27017/mydb";
 
 // importing user modules
 var orderUtil = require('./orderUtil.js');
 var mongoUtil = require('./mongoUtil.js');
 var miscUtil = require('./miscUtil.js');
 var coincapApiUtil = require('./coincapApiUtil.js');
-var display = require('./display.js');
 
 
+var port = 8080;
+var app = express();
+const server = app.listen(port, () => {
+    console.log("Listening on port: " + port);
+});
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('A user is connected');
+
+    mongoUtil.sendPastOrders(io); // it sends past orders to frontend using socket.io
+    sendUserData();
+});
+
+// there is only 1 user for now
 const userId = 1;
+
 var currentFiat = 1000;
 var holdings = {"BTCUSD": 0};  // stores the value in its respective coin not in dollars
-// shortedHolding should store the amt of BTC and also the price then
-var sortedHoldings = {"BTCUSD": []}; // stores the value in its respective coin not in dollars
-var jsonUserData = {"userId":userId, "currentFiat":currentFiat, "holdings":holdings, "sortedHoldings": sortedHoldings};
-mongoUtil.updateUserdataToDb(jsonUserData);
+var sortedHoldings = {"BTCUSD": []};  // shortedHolding stores the amt of BTC and also the price when trade happened
+var defaultUserData = {"userId":userId, "currentFiat":currentFiat, "holdings":holdings, "sortedHoldings": sortedHoldings};
 
 
-orderUtil.updateOrders(); // check if previous orders was completed, updates the current Amt accordingly
+var userData;
+async function fetchUserData() {
+    const client = await MongoClient.connect(mongoDbUrl, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    });
+    const db = client.db('mainDb');
+    // execute find query
+    const items = await db.collection('userData').find({userId : userId}).toArray();
+    // if user already exists
+    if(items.length) {
+        userData = items[0];
+    }
+
+    else {
+        userData = defaultUserData;
+        mongoUtil.updateUserdataToDb(userData);
+    }
+    console.log("user data are ", userData);
+
+    client.close();
+}
+fetchUserData();
+
+
+
+// orderUtil.updateOrders(); // check if previous orders was completed, updates the current Amt accordingly
 coincapApiUtil.startWebsocket(); // keeps listening, helps to get current price synchronously
-display.displayOrders(); // shows all order present in the database
 
 
-var app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
 // this will be run when submit is clicked, i.e any order inserted
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
 app.post('/formData', urlencodedParser, function (req, res) {
 
     // websocket is still loading so dont take any order
@@ -38,69 +76,52 @@ app.post('/formData', urlencodedParser, function (req, res) {
     }
     
     else {
-        // jsonData i.e the order json
-        var jsonData = miscUtil.addMetaData(req.body, userId, currentFiat);
-
-        console.log(jsonData);
-
-        mongoUtil.addOrderToDb(jsonData);
-        display.displayOrders();
-
-        var currentPrice = coincapApiUtil.getCurrentPrice();
-        console.log("current price when trade happened ", coincapApiUtil.getCurrentPrice());
-
-        // TODO: these  tradde decisions should be made in orderutil.js, buybelow/buyAbove 
-        //      should have their decision and only then order is placed and current cash is reduced. 
-        var buyAmtIds = ['buyNowAmt', 'buyBelowAmt', 'buyAboveAmt'];
-        for(let i=0; i<buyAmtIds.length; i++) {
-            if(buyAmtIds[i] in jsonData){
-                currentFiat-= jsonData[buyAmtIds[i]];
-                holdings["BTCUSD"] += jsonData[buyAmtIds[i]]/currentPrice;
-                console.log("current cash changed to ", currentFiat);
-                console.log("new btc holding ", jsonData[buyAmtIds[i]]/currentPrice);
-                break;
-            }
+        async function foo() {
+            let orderData = miscUtil.addMetaOrderData(req.body, userId);
+            orderUtil.executeOrders(orderData, userData);
+            await mongoUtil.addOrderToDb(orderData); // await for the orders to be inserted to DB only then send them to frontend
+            mongoUtil.sendPastOrders(io);
         }
-        // TODO: if found in above buyAmt then skip below
-        var sortAmtIds = ['sortNowAmt', 'sortBelowAmt', 'sortAboveAmt'];
-        for(let i=0; i<sortAmtIds.length; i++) {
-            if(sortAmtIds[i] in jsonData){
-                currentFiat-= jsonData[sortAmtIds[i]];
-                let temp = [jsonData[sortAmtIds[i]]/currentPrice, currentPrice];
-                sortedHoldings["BTCUSD"].push(temp);
-                console.log("current cash changed to ", currentFiat);
-                console.log("new sorted btc holding ", jsonData[sortAmtIds[i]]/currentPrice);
-                break;
-            }
-        }
+        foo();
+
     }
     
     res.end();
 });
 
-
-var port = 8080;
-
-const server = app.listen(port, () => {
-    console.log("Listening on port: " + port);
+app.post('/resetBtn', function (req, res) {
+    async function foo() {
+        console.log("userMataData and pastOrders were updated to default");
+        userData = defaultUserData;
+        mongoUtil.updateUserdataToDb(userData);
+        await mongoUtil.deletePastOrders(); // await for the pastOrders to be deleted only then send it to frontend 
+        mongoUtil.sendPastOrders(io);
+    }
+    foo();
+    res.end();
 });
-const io = require('socket.io')(server);
 
 
-// when socket is connected start sending current price and other userMetadata to frontend
-io.on('connection', (socket) => {
-    // console.log('a user connected');
+function sendUserData() {
     
+    // connect to websocket which sends current price
+    // whenever received, send it to frontend using socket.io
+
     var websocketURL = 'wss://ws.coincap.io/prices?assets=bitcoin';
     const ws = new WebSocket(websocketURL);
+
+    ws.onerror = function(event) {
+        console.error("WebSocket error observed ;(");
+    };
+
     ws.on('message', function incoming(data) {
         var currentPriceJson = JSON.parse(data);
-        // console.log(typeof(currentPriceJson));
-        let userMetadata = {"currentPrice": currentPriceJson, "currentFiat": currentFiat, holdings, sortedHoldings};
-        io.emit("userMetadata", userMetadata);
-        // console.log(userMetadata);
+        // console.log("going ", currentPriceJson);
+        let tempUserData = JSON.parse(JSON.stringify(userData));
+        tempUserData["currentPrice"] = currentPriceJson;
+        io.emit("userData", tempUserData);
     });
-});
+}
 
 // send/load html and javascript something 
 app.use(express.static(__dirname + '/public'));
