ace435fccfe9c01b03be2bd46f291d719a89512c
mightomi <meightpc@gmail.com>
2021-12-08 14:44:49 +0530

    rerender Widget only on sybmol change

--- client/src/components/Home/Widget.js
+++ client/src/components/Home/Widget.js
@@ -1,34 +1,32 @@
-import React from 'react'
-import TradingViewWidget, { Themes } from 'react-tradingview-widget';
-import './Widget.css'
+import React from "react";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
+import "./Widget.css";
 
-function Widget(sybmol) {
-  
-    return (
-        <div className = "widget-class">
-           <TradingViewWidget 
-            symbol="BINANCE:BTCUSDT" 
+function Widget(props) {
+  return (
+    <div className="widget-class">
+      <TradingViewWidget
+        symbol="BINANCE:BTCUSDT"
+        
+        width={850}
+        height={500}
 
-            width = "850"
-            height = "500"
+        timezone="Asia/Kolkata"
+        locale="in"
 
-            timezone = "Asia/Kolkata"
-            locale = "in"
+        style="2"
+        theme={Themes.DARK}
+        range="12m"
 
-            style = "2"
-            theme={Themes.DARK}
-            range = "12M"
-
-
-            // toolbar_bg = "#f1f3f6"
-            allow_symbol_change = {false}
-            details = {true}
-            studies = {["MASimple@tv-basicstudies"]}
-            // hotlist = {true}
-            // calendar = {true}
-           />
-        </div>
-    )
+        // toolbar_bg = "#f1f3f6"
+        allow_symbol_change={false}
+        details={true}
+        studies={["MASimple@tv-basicstudies"]}
+        // hotlist = {true}
+        // calendar = {true}
+      />
+    </div>
+  );
 }
 
-export default Widget
+export default React.memo(Widget);
