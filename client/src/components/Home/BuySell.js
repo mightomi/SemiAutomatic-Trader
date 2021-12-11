680c39317c1aad210170339fc0a068350e512f9f
dark-faze <68679017+dark-faze@users.noreply.github.com>
2021-12-11 11:13:40 +0530

    added buy,sell modals

--- client/src/components/Home/BuySell.js
+++ client/src/components/Home/BuySell.js
@@ -12,12 +12,52 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
+//Modal Styles
+const style = {
+  modal: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "black",
+    color: "white",
+    border: "2px solid #FFFFFF",
+    boxShadow: 24,
+    p: 4,
+  },
+  list: {
+    width: "100%",
+    height: "50%",
+    maxWidth: 360,
+    color: "white",
+    bgcolor: "black",
+    borderRadius: 3,
+  },
+  dropList: {
+    color: "black",
+    bgcolor: "white",
+    borderRadius: "5px",
+    marginTop: "15px",
+    marginBottom: "10px",
+  },
+};
 
 export default function BuySell(props) {
   const [coin, setCoin] = React.useState("");
   const handleChange = (event) => {
     setCoin(event.target.value);
   };
+
+  const [buyopen, setBuyOpen] = useState(false);
+  const [sellopen, setSellOpen] = useState(false);
+  const handleBuyOpen = () => setBuyOpen(true);
+  const handleSellOpen = () => setSellOpen(true);
+
+  const [orderType, setOrderType] = useState("");
 
   return (
     <div className="BuySell-div">
@@ -38,16 +78,7 @@ export default function BuySell(props) {
           </Select>
         </FormControl>
       </div>
-      <List
-        sx={{
-          width: "100%",
-          height: "50%",
-          maxWidth: 360,
-          color: "white",
-          bgcolor: "black",
-          borderRadius: 3,
-        }}
-      >
+      <List sx={style.list}>
         <ListItem>
           <ListItemAvatar>
             <ListItemAvatar>
@@ -87,19 +118,46 @@ export default function BuySell(props) {
         </ListItem>
       </List>
 
-      <div className="button-row">
+      {/*Buy Sell Sort Buttons Row Starts here*/}
 
+      <div className="button-row">
+        {/*Buy Button*/}
         <button
           onClick={() => {
+            handleBuyOpen();
             props.placeOrder(null);
           }}
           className="button green-button"
         >
           <span>Buy</span>
         </button>
+        <Modal open={buyopen} onClose={() => setBuyOpen(false)}>
+          <Box sx={style.modal}>
+            <h1>Buy Modal text</h1>
+            <FormControl sx={style.dropList} fullWidth>
+              <InputLabel id="demo-simple-select-label">Order Type</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={orderType}
+                label="Type"
+                onChange={(e) => {
+                  setOrderType(e.target.value);
+                  // console.log(orderType);
+                }}
+              >
+                <MenuItem value={"limit"}>Limit</MenuItem>
+                <MenuItem value={"mp"}>Market Price</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Modal>
+
+        {/*Sell Button*/}
 
         <button
           onClick={() => {
+            handleSellOpen();
             // props.placeOrder();
             console.log("called sell");
           }}
@@ -107,7 +165,36 @@ export default function BuySell(props) {
         >
           <span>Sell</span>
         </button>
+        <Modal open={sellopen} onClose={() => setSellOpen(false)}>
+          <Box sx={style.modal}>
+            <h1>Sell Modal text</h1>
+            <FormControl sx={style.dropList} fullWidth>
+              <InputLabel id="demo-simple-select-label">Order Type</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={orderType}
+                label="Type"
+                onChange={handleChange}
+              >
+                <MenuItem value={"limit"}>Limit</MenuItem>
+                <MenuItem value={"mp"}>Market Price</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Modal>
 
+        {/*Sort Button*/}
+
+        <button
+          onClick={() => {
+            // props.placeOrder();
+            console.log("called sell");
+          }}
+          className="button blue-button"
+        >
+          <span>Sort</span>
+        </button>
       </div>
     </div>
   );
